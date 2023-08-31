7de472570174f34a97d090b882d6ac8ebf1d83fb
Valery Gorbanev <115099825+vgartg@users.noreply.github.com>
2023-08-31 11:53:08 +0300

    update index.ts 31.08

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,11 @@ import { Address } from "ton-core";
 import { sign } from "ton-crypto";
 import { toNano } from "ton-core";
 import { TonClient } from "ton";
+import { getHttpEndpoint } from "@orbs-network/ton-access";
 
 
 async function main() {
-  const mnemonicArray = await mnemonicNew(24); // 24 is the number of words in a seed phrase
+  const mnemonicArray = 'huge another such vote kite middle quality delay attract lazy era add army rare melt talent sail opinion gossip curtain visual estate best speed'.split(' ')
   const keyPair = await mnemonicToWalletKey(mnemonicArray); // extract private and public keys from mnemonic
   console.log(mnemonicArray) // if we want, we can print our mnemonic
 
@@ -19,29 +20,29 @@ async function main() {
   const result = await compileFunc({
     targets: ['wallet_v3.fc'], // targets of your project
     sources: {
-      "stdlib.fc": fs.readFileSync('./src/stdlib.fc', { encoding: 'utf-8' }),
-      "wallet_v3.fc": fs.readFileSync('./src/wallet_v3.fc', { encoding: 'utf-8' }),
+        "stdlib.fc": fs.readFileSync('./src/stdlib.fc', { encoding: 'utf-8' }),
+        "wallet_v3.fc": fs.readFileSync('./src/wallet_v3.fc', { encoding: 'utf-8' }),
     }
-  });
-
-  if (result.status === 'error') {
+    });
+    
+    if (result.status === 'error') {
     console.error(result.message)
     return;
-  }
-
-  const codeCell = Cell.fromBoc(Buffer.from(result.codeBoc, "base64"))[0]; // get buffer from base64 encoded BOC and get cell from this buffer
-
-  // now we have base64 encoded BOC with compiled code in result.codeBoc
-  console.log('Code BOC: ' + result.codeBoc);
-  console.log('\nHash: ' + codeCell.hash().toString('base64')); // get the hash of cell and convert in to base64 encoded string. We will need it further
+    }
+    
+    const codeCell = Cell.fromBoc(Buffer.from(result.codeBoc, "base64"))[0]; // get buffer from base64 encoded BOC and get cell from this buffer
+    
+    // now we have base64 encoded BOC with compiled code in result.codeBoc
+    console.log('Code BOC: ' + result.codeBoc);
+    console.log('\nHash: ' + codeCell.hash().toString('base64')); // get the hash of cell and convert in to base64 encoded string. We will need it further
 
   const dataCell = beginCell().
-    storeUint(0, 32). // Seqno
-    storeUint(698983191, 32). // Subwallet ID
-    storeBuffer(keyPair.publicKey). // Public Key
-    endCell();
+  storeUint(0, 32). // Seqno
+  storeUint(698983191, 32). // Subwallet ID
+  storeBuffer(keyPair.publicKey). // Public Key
+  endCell();
 
-  const stateInit = beginCell().
+    const stateInit = beginCell().
     storeBit(0). // No split_depth
     storeBit(0). // No special
     storeBit(1). // We have code
@@ -50,38 +51,38 @@ async function main() {
     storeRef(dataCell).
     storeBit(0). // No library
     endCell();
-
+  
   const contractAddress = new Address(0, stateInit.hash()); // get the hash of stateInit to get the address of our smart contract in workchain with ID 0
   console.log(`Contract address: ${contractAddress.toString()}`); // Output contract address to console
 
   const internalMessageBody = beginCell().
-    storeUint(0, 32).
-    storeStringTail("Hello, TON!").
-    endCell();
+  storeUint(0, 32).
+  storeStringTail("Hello, TON!").
+  endCell();
 
-  const internalMessage = beginCell().
-    storeUint(0x10, 6). // no bounce
-    storeAddress(Address.parse("kQDo2XD-pkY7X_b06JiGQxu_DGZiOpiE6xtaxWEyKclvTpNm")).
-    storeCoins(toNano("0.03")).
-    storeUint(1, 1 + 4 + 4 + 64 + 32 + 1 + 1). // We store 1 that means we have body as a reference
-    storeRef(internalMessageBody).
-    endCell();
+const internalMessage = beginCell().
+  storeUint(0x10, 6). // no bounce
+  storeAddress(Address.parse("kQDgm5jA3lE9GrXECinqZA6pnJasEf0X8pqs1-j37lXOXofr")).
+  storeCoins(toNano("0.03")).
+  storeUint(1, 1 + 4 + 4 + 64 + 32 + 1 + 1). // We store 1 that means we have body as a reference
+  storeRef(internalMessageBody).
+  endCell();
 
-  // transaction for our wallet
-  const toSign = beginCell().
-    storeUint(subWallet, 32).
-    storeUint(Math.floor(Date.now() / 1e3) + 60, 32).
-    storeUint(0, 32). // We put seqno = 0, because after deploying wallet will store 0 as seqno
-    storeUint(3, 8).
-    storeRef(internalMessage);
+// transaction for our wallet
+const toSign = beginCell().
+  storeUint(subWallet, 32).
+  storeUint(Math.floor(Date.now() / 1e3) + 60, 32).
+  storeUint(0, 32). // We put seqno = 0, because after deploying wallet will store 0 as seqno
+  storeUint(3, 8).
+  storeRef(internalMessage);
 
-  const signature = sign(toSign.endCell().hash(), keyPair.secretKey);
-  const body = beginCell().
-    storeBuffer(signature).
-    storeBuilder(toSign).
-    endCell();
+const signature = sign(toSign.endCell().hash(), keyPair.secretKey);
+const body = beginCell().
+  storeBuffer(signature).
+  storeBuilder(toSign).
+  endCell();
 
-  const externalMessage = beginCell().
+    const externalMessage = beginCell().
     storeUint(0b10, 2). // indicate that it is an incoming external transaction
     storeUint(0, 2). // src -> addr_none
     storeAddress(contractAddress).
@@ -93,15 +94,15 @@ async function main() {
     storeRef(body). // Store Message Body as a reference
     endCell();
 
-
-    const client = new TonClient({
-      endpoint: "https://testnet.toncenter.com/api/v2/",
-      apiKey: "be3bc142cd29d2289366c7b1121f955dce19daecd7c53fe0da278ac85d0544e9" // you can get an api key from @tonapibot bot in Telegram
+    // get the decentralized RPC endpoint
+    const endpoint = await getHttpEndpoint({
+      network: "testnet",
     });
+
+    // initialize ton library
+    const client = new TonClient({ endpoint });
     
     client.sendFile(externalMessage.toBoc());
-
 }
-
 
 main().finally(() => console.log("Exiting..."));
